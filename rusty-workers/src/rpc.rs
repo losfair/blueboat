62658132eab20d74230900d689295cc2c5605e4c
losfair <zhy20000919@hotmail.com>
2021-01-04 14:49:25 +0800

    Fetch definition & retries

--- rusty-workers/src/rpc.rs
+++ rusty-workers/src/rpc.rs
@@ -5,10 +5,11 @@ use crate::types::*;
 macro_rules! impl_connect {
     ($ty:ident) => {
         impl $ty {
-            pub async fn connect<A: tokio::net::ToSocketAddrs>(addr: A) -> GenericResult<Self> {
-                let mut transport = tarpc::serde_transport::tcp::connect(addr, crate::SerdeFormat::default);
-                transport.config_mut().max_frame_length(16 * 1048576);
-                let client = $ty::new(tarpc::client::Config::default(), transport.await?).spawn()?;
+            pub async fn connect<A: tokio::net::ToSocketAddrs + Clone + Send + Sync + Unpin + 'static>(addr: A) -> GenericResult<Self> {
+                use stubborn_io::{StubbornTcpStream};
+                let tcp_stream = StubbornTcpStream::connect(addr).await?;
+                let transport = tarpc::serde_transport::Transport::from((tcp_stream, crate::SerdeFormat::default()));
+                let client = $ty::new(tarpc::client::Config::default(), transport).spawn()?;
                 Ok(client)
             }
         }
@@ -24,7 +25,6 @@ macro_rules! impl_listen {
                 use $crate::futures::StreamExt;
                 use $crate::tarpc::server::Channel;
                 let mut listener = $crate::tarpc::serde_transport::tcp::listen(addr, $crate::SerdeFormat::default).await?;
-                listener.config_mut().max_frame_length(16 * 1048576);
                 listener
                     .filter_map(|r| $crate::futures::future::ready(r.ok())) // Ignore accept errors.
                     .map($crate::tarpc::server::BaseChannel::with_defaults)
@@ -58,3 +58,14 @@ pub trait RuntimeService {
 }
 
 impl_connect!(RuntimeServiceClient);
+
+/// Fetch service.
+#[tarpc::service]
+pub trait FetchService {
+    /// Fetch from a remote URL.
+    /// 
+    /// Result is wrapped twice because we want to be able to send custom errors to client.
+    async fn fetch(req: RequestObject) -> GenericResult<Result<ResponseObject, String>>;
+}
+
+impl_connect!(FetchServiceClient);
