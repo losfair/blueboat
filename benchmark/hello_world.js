68bbe065e1b4b04ad334bb345308834413d20a57
losfair <zhy20000919@hotmail.com>
2021-01-11 19:29:29 +0800

    Remove outdated benchmark

--- benchmark/hello_world.js
+++
@@ -1,9 +0,0 @@
-console.log("hello_world loaded");
-
-addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
-});
-
-function handleRequest(req) {
-    return new Response("Hello, world! Request:" + JSON.stringify(req));
-}
