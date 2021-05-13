df33d34d97b70c2c3d0c00dc2373c5e3cdc2720f
losfair <zhy20000919@hotmail.com>
2021-05-13 21:01:59 +0800

    applog migration

---
+++ sql/migration_v1/applog.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `applog` (
+  `appid` VARCHAR(64) NOT NULL ,
+  `logtime` BIGINT UNSIGNED NOT NULL ,
+  `subid` INT UNSIGNED NOT NULL ,
+  `logcontent` TEXT NOT NULL )
+  CHARSET=utf8mb4 COLLATE utf8mb4_bin;
+
+ALTER TABLE `applog` ADD INDEX `time_range_query` (`appid`, `logtime`);
+
+ALTER TABLE `applog` ADD PRIMARY KEY (`appid`, `logtime`, `subid`);
