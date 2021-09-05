9ac7cf1db1482b6d7e337f94734a578ede1a1fb2
losfair <zhy20000919@hotmail.com>
2021-09-05 14:48:47 +0000

    Merge migration scripts

--- sql/migration_000001/bundles.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `bundles` (
-  `id` VARCHAR(64) NOT NULL ,
-  `bundle` LONGBLOB NOT NULL ,
-  `createtime` BIGINT UNSIGNED NOT NULL ,
-  PRIMARY KEY (`id`))
-  CHARSET=utf8mb4 COLLATE utf8mb4_bin;
-
-ALTER TABLE `bundles` ADD INDEX (`createtime`);
