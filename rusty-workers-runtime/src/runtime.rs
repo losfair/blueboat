7364f342bcca27eb616e9ba5ab844219341a9ee8
losfair <zhy20000919@hotmail.com>
2021-09-05 09:09:12 +0000

    Pin Rust toolchain version.

--- rusty-workers-runtime/src/runtime.rs
+++ rusty-workers-runtime/src/runtime.rs
@@ -43,7 +43,7 @@ pub struct InstanceStatistics {
 }
 
 pub fn init() {
-    let platform = v8::new_default_platform().unwrap();
+    let platform = v8::new_default_platform(0, false).make_shared();
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 }
