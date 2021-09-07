70595765c9e54740e9bed1d5aa3a79bc34dec25a
losfair <zhy20000919@hotmail.com>
2021-09-07 07:05:17 +0000

    Make more k8s options configurable

--- k8s_rewrite.sh
+++ k8s_rewrite.sh
@@ -10,18 +10,31 @@ if [ ! -f "$CONFIG" ]; then
     exit 1
 fi
 
-if [ -z "$SUFFIX" ]; then
-    echo "[-] suffix required"
-    exit 1
-fi
-
 . "$CONFIG"
 
-# Allow empty suffix
-#if [ -z "$IMAGE_SUFFIX" ]; then
-#    echo "[-] IMAGE_SUFFIX not defined"
-#    exit 1
-#fi
+set +u
+
+if [ -z "$RW_HIGH_MEMORY_THRESHOLD_BYTES" ]; then
+    RW_HIGH_MEMORY_THRESHOLD_BYTES=536870912
+fi
+
+if [ -z "$RW_ISOLATE_POOL_SIZE" ]; then
+    RW_ISOLATE_POOL_SIZE=70
+fi
+
+if [ -z "$RW_MAX_NUM_OF_INSTANCES" ]; then
+    RW_MAX_NUM_OF_INSTANCES=60
+fi
+
+if [ -z "$RW_MAX_CONCURRENT_REQUESTS" ]; then
+    RW_MAX_CONCURRENT_REQUESTS=56
+fi
+
+if [ -z "$RW_EXECUTION_CONCURRENCY" ]; then
+    RW_EXECUTION_CONCURRENCY=8
+fi
+
+set -u
 
 rm -r "./k8s.$SUFFIX" || true
 cp -r "`dirname $0`/k8s" "./k8s.$SUFFIX"
@@ -36,6 +49,11 @@ find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__NUM_PROXIES__#$NUM_PROXIES
 find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__NUM_RUNTIMES__#$NUM_RUNTIMES#g" '{}' ';'
 find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__NUM_FETCHD__#$NUM_FETCHD#g" '{}' ';'
 find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__CPU_REQUEST_PER_RUNTIME__#$CPU_REQUEST_PER_RUNTIME#g" '{}' ';'
+find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__RW_HIGH_MEMORY_THRESHOLD_BYTES__#$RW_HIGH_MEMORY_THRESHOLD_BYTES#g" '{}' ';'
+find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__RW_ISOLATE_POOL_SIZE__#$RW_ISOLATE_POOL_SIZE#g" '{}' ';'
+find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__RW_MAX_NUM_OF_INSTANCES__#$RW_MAX_NUM_OF_INSTANCES#g" '{}' ';'
+find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__RW_MAX_CONCURRENT_REQUESTS__#$RW_MAX_CONCURRENT_REQUESTS#g" '{}' ';'
+find "./k8s.$SUFFIX" -name "*.yaml" -exec sed -i "s#__RW_EXECUTION_CONCURRENCY__#$RW_EXECUTION_CONCURRENCY#g" '{}' ';'
 
 set +u
 if [ -z "$IMAGE_PULL_SECRET" ]; then
