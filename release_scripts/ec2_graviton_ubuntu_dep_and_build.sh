2c99385f74939cc76f3549590d261b72619c3b2b
losfair <zhy20000919@hotmail.com>
2021-10-26 08:13:07 +0000

    Add EC2 Graviton build script.

---
+++ release_scripts/ec2_graviton_ubuntu_dep_and_build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script bootstraps a Graviton EC2 instance with Ubuntu 20.04 and builds Blueboat on it.
+
+set -euxo pipefail
+
+sudo apt update
+sudo apt install -y \
+  git libseccomp-dev build-essential libsqlite3-dev \
+  pkg-config libssl-dev libclang-dev clang libz3-dev \
+  python2 python ninja-build libfontconfig1-dev
+
+git clone https://github.com/losfair/blueboat
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+. ~/.cargo/env
+. ~/.nvm/nvm.sh
+nvm install 16
+cargo install cargo-deb
+curl -f https://get.pnpm.io/v6.16.js | node - add --global pnpm
+cd blueboat
+cd jsland
+pnpm i
+cd ..
+SKIA_NINJA_COMMAND=/usr/bin/ninja BLUEBOAT_DEB=1 ./build.sh
