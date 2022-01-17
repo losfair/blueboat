b928c2413ad2e17da7bb2e4e12593b6144082946
losfair <zhy20000919@hotmail.com>
2022-01-17 04:29:25 +0000

    Mimalloc in CI

--- docker/run.sh
+++ docker/run.sh
@@ -6,7 +6,8 @@ if [ -z "$SMRAPP_BLUEBOAT_FONT_DIR" ]; then
   export SMRAPP_BLUEBOAT_FONT_DIR="/opt/blueboat/fonts"
 fi
 
-exec /usr/bin/blueboat_server -l "$LISTEN_ADDR" \
+export LD_PRELOAD="/libmimalloc.so"
+/usr/bin/blueboat_server -l "$LISTEN_ADDR" \
   --s3-bucket "$S3_BUCKET" --s3-region "$S3_REGION" \
   --s3-endpoint "$S3_ENDPOINT" \
   --db "$MYSQL_CONN_STRING" \
