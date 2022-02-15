f5f0b271a58a06d9d6d0721b2df71dea78c58c92
losfair <zhy20000919@hotmail.com>
2022-02-15 07:14:11 +0000

    HTML fragments; improve tests

--- jsland/src/text/dom/index.ts
+++ jsland/src/text/dom/index.ts
@@ -1,5 +1,9 @@
 import { HostObject } from "../../host_object";
 
+export interface HtmlParseOptions {
+  fragment?: boolean,
+}
+
 export class GenericDOM extends HostObject {
   protected constructor(raw: symbol) {
     super(raw);
@@ -7,8 +11,8 @@ export class GenericDOM extends HostObject {
 }
 
 export class HtmlDOM extends GenericDOM {
-  constructor(text: string | Uint8Array) {
-    const sym = <symbol>__blueboat_host_invoke("text_dom_html_parse", text);
+  constructor(text: string | Uint8Array, opts?: HtmlParseOptions) {
+    const sym = <symbol>__blueboat_host_invoke("text_dom_html_parse", text, opts);
     super(sym);
   }
 
--- src/api/text/dom/mod.rs
+++ src/api/text/dom/mod.rs
@@ -3,9 +3,11 @@ use std::rc::Rc;
 use anyhow::Result;
 use html5ever::tendril::TendrilSink;
 use markup5ever_rcdom::{RcDom, SerializableHandle};
+use serde::Deserialize;
 use v8;
 
 use crate::{
+  api::util::v8_deserialize,
   registry::SymbolRegistry,
   v8util::{create_uint8array_from_bytes, LocalValueExt},
 };
@@ -14,18 +16,42 @@ pub struct Dom {
   rcd: RcDom,
 }
 
+#[derive(Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+struct HtmlParseOptions {
+  #[serde(default)]
+  fragment: bool,
+}
+
 pub fn api_dom_html_parse(
   scope: &mut v8::HandleScope,
   args: v8::FunctionCallbackArguments,
   mut retval: v8::ReturnValue,
 ) -> Result<()> {
   let text = unsafe { args.get(1).read_string_assume_noalias(scope)? };
-  let rcd = html5ever::parse_document(
-    RcDom::default(),
-    html5ever::ParseOpts {
-      ..Default::default()
-    },
-  )
+  let opts = args.get(2);
+  let opts: HtmlParseOptions = if opts.is_null_or_undefined() {
+    Default::default()
+  } else {
+    v8_deserialize(scope, opts)?
+  };
+  let parse_opts = html5ever::ParseOpts {
+    ..Default::default()
+  };
+  let rcd = if opts.fragment {
+    html5ever::parse_fragment(
+      RcDom::default(),
+      parse_opts,
+      markup5ever::QualName::new(
+        None,
+        html5ever::Namespace::from("http://www.w3.org/1999/xhtml"),
+        html5ever::LocalName::from("body"),
+      ),
+      vec![],
+    )
+  } else {
+    html5ever::parse_document(RcDom::default(), parse_opts)
+  }
   .from_utf8()
   .read_from(&mut text.as_bytes())
   .map_err(|e| anyhow::Error::from(e).context("failed to parse html document"))?;
@@ -66,7 +92,11 @@ pub fn api_dom_html_serialize(
   html5ever::serialize::serialize(
     &mut bytes,
     &SerializableHandle::from(dom.rcd.document.clone()),
-    html5ever::serialize::SerializeOpts::default(),
+    html5ever::serialize::SerializeOpts {
+      // prevent panic
+      create_missing_parent: true,
+      ..Default::default()
+    },
   )
   .map_err(|e| anyhow::Error::from(e).context("html serialization failed"))?;
   retval.set(create_uint8array_from_bytes(scope, &bytes).into());
@@ -99,13 +129,47 @@ mod tests {
     let mut tester = ApiTester::new();
     let out: String = tester.run_script(
       r#"
-let dom = new TextUtil.DOM.HtmlDOM("<!DOCTYPE html><html><body></body></html>");
-new TextDecoder().decode(dom.serialize());
+{
+  let dom = new TextUtil.DOM.HtmlDOM("<!DOCTYPE html><html><body></body></html>");
+  new TextDecoder().decode(dom.serialize());
+}
     "#,
     );
     assert_eq!(
       out.as_str(),
       "<!DOCTYPE html><html><head></head><body></body></html>"
+    );
+    let out: String = tester.run_script(
+      r#"
+{
+  let dom = new TextUtil.DOM.HtmlDOM("<p>hello</p>", { fragment: true });
+  new TextDecoder().decode(dom.serialize());
+}
+    "#,
+    );
+    assert_eq!(out.as_str(), "<html><p>hello</p></html>");
+  }
+
+  #[test]
+  fn test_xml_roundtrip() {
+    let mut tester = ApiTester::new();
+    let out: String = tester.run_script(
+      r#"
+{
+  let dom = new TextUtil.DOM.XmlDOM(`
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel><title>A</title></channel>
+  <channel><title>B</title></channel>
+</rss>
+`);
+  new TextDecoder().decode(dom.serialize());
+}
+    "#,
+    );
+    assert_eq!(
+      out.as_str(),
+      "<?xml version=\"1.0\" encoding=\"utf-8\"?><rss version=\"2.0\">\n  <channel><title>A</title></channel>\n  <channel><title>B</title></channel>\n</rss>"
     );
   }
 }
