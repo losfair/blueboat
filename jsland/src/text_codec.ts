8019a0832c5fa80c215834502d71b05f3c4aba03
losfair <zhy20000919@hotmail.com>
2022-02-21 17:09:16 +0000

    Allow 0-arg calls to TextEncoder/TextDecoder APIs

--- jsland/src/text_codec.ts
+++ jsland/src/text_codec.ts
@@ -1,11 +1,13 @@
 export class TextEncoder {
-  encode(s: string): Uint8Array {
+  encode(s?: string): Uint8Array {
+    if(!s) return new Uint8Array();
     return <Uint8Array>__blueboat_host_invoke("encode", s);
   }
 }
 
 export class TextDecoder {
-  decode(s: Uint8Array): string {
+  decode(s?: Uint8Array): string {
+    if(!s) return "";
     return <string>__blueboat_host_invoke("decode", s);
   }
 }
