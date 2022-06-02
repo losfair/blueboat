d05696470ad1671b6dee85241480ea9e5e9e6a26
losfair <zhy20000919@hotmail.com>
2022-06-02 15:06:57 +0000

    update build_pkg

--- examples/build_pkg.sh
+++ examples/build_pkg.sh
@@ -8,5 +8,5 @@ tar c . > ../pkg.tar
 cd ..
 
 RUST_LOG=debug S3CMD_CFG=~/blueboat_test_s3.creds ../scripts/build_and_upload.mjs \
-  -f ./pkg.tar --s3_bucket usw-blueboat-apps --s3_prefix pkg/ \
+  -f ./pkg.tar --s3_bucket apps --s3_prefix pkg/ \
   --kv ./pkg.kv.json
